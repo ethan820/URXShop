575bead72d2e4d0dd005454c5ac024d44c76a5de
David Lee <davidhollandlee@gmail.com>
2013-10-31 15:45:09 -0700

    clean slate

--- URXShop/Classes/AppDelegate.m
+++ URXShop/Classes/AppDelegate.m
@@ -6,60 +6,26 @@
 //
 
 #import "AppDelegate.h"
-#import <Turnpike/Turnpike.h>
 #import "ProductsViewController.h"
 #import "ProductViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [Parse setApplicationId:@"zIFzjuA3H4AV9W6wUTUCL8wAmkdwknfqsYJ3i1DA"clientKey:@"uYF9AGQhMCbZfKJSYQUye4FQSqk7SLTE11ml540m"];
-    
-    UITabBarController *tabBarController = (UITabBarController *)self.window.rootViewController;
-    
-    
-    
-    // Turnpike Routing:
-    [Turnpike mapRoute:@"hello" ToDestination:^(TPRouteRequest *request) {
-        NSLog(@"Hello World!");
-    }];
-    
-    [Turnpike mapRoute:@"login" ToDestination:^(TPRouteRequest *request) {
-        [tabBarController setSelectedIndex:0];
-    }];
-    
-    [Turnpike mapRoute:@"product/:product_id" ToDestination:^(TPRouteRequest *request) {
-        NSString *product_id = [request.routeParameters valueForKey:@"product_id"];
-        if (product_id) {
-            [tabBarController setSelectedIndex:0];
-            [[NSNotificationCenter defaultCenter] postNotificationName:@"com.urx.shop.productsViewController.urxLinkToProduct" object:nil userInfo:[NSDictionary dictionaryWithObject:product_id forKey:@"product_id"]];
-        }
-    }];
-    
-    [Turnpike mapRoute:@"shopping_cart" ToDestination:^(TPRouteRequest *request) {
-        [tabBarController setSelectedIndex:1];
-    }];
-    
-    
+    [Parse setApplicationId:@"YourParseApplicationID"clientKey:@"YourParseClientAPIKey"];
     
     return YES;
 }
 
 // iOS >= 4.2
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
-    [Turnpike resolveURL:url];
     return YES;
 }
 
 // iOS < 4.2
 - (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
-    [Turnpike resolveURL:url];
     return YES;
 }
-                                                                                                                                                 
-                                                                                                                                                 
-                                                                                                                                                 
-                                                                                                                                                 
-                                                                                                                                                 
+
                                                                                                                                                  
 @end
