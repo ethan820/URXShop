a7e2967376cdd375767406b29a256793c4860aa9
ethan820 <ethan820@ethan820deair.home>
2014-03-11 22:01:02 +0800

    1.0.0 version  Please enter the commit message for your changes. Lines starting

---
+++ URXShop/Classes/UILabel+heightToFit.h
@@ -0,0 +1,16 @@
+//
+//  UILabel+heightToFit.h
+//  shareShopping
+//
+//  Created by Mac on 14/1/16.
+//  Copyright (c) 2014年 EZTABLE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (heightToFit)
+
+- (CGFloat) heightToFit;
+- (CGFloat) heightToFitWithString:(NSString *)string;
+
+@end
---
+++ URXShop/Classes/UILabel+heightToFit.m
@@ -0,0 +1,44 @@
+//
+//  UILabel+heightToFit.m
+//  shareShopping
+//
+//  Created by Mac on 14/1/16.
+//  Copyright (c) 2014年 EZTABLE. All rights reserved.
+//
+
+#import "UILabel+heightToFit.h"
+
+@implementation UILabel (heightToFit)
+
+-(CGFloat)heightToFit{
+    return [self heightToFitWithString:self.text];
+}
+-(CGFloat)heightToFitWithString:(NSString *)string
+{
+    if (string==nil) {
+        return 0.0;
+    }
+    CGFloat height;
+    
+    NSMutableParagraphStyle *style  = [[NSMutableParagraphStyle alloc] init];
+    style.lineHeightMultiple = 1.5;
+    self.attributedText = [[NSAttributedString alloc] initWithString:string attributes:@{NSParagraphStyleAttributeName:style, NSFontAttributeName:self.font}];
+    
+    if (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_6_1) {
+        
+        CGRect textRect = [string boundingRectWithSize:CGSizeMake(self.width, 1000) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSParagraphStyleAttributeName:style, NSFontAttributeName:self.font} context:nil];
+        
+        height = textRect.size.height;
+    }else{
+        CGFloat fontSize = self.font.pointSize;
+        CGSize textSize = [string sizeWithFont:[UIFont systemFontOfSize:fontSize] constrainedToSize:CGSizeMake(self.width, 1000) lineBreakMode:NSLineBreakByWordWrapping];
+//        NSInteger numberOfLines = textSize.height/fontSize;
+        height = textSize.height*style.lineHeightMultiple;
+    }
+//    NSLog(@"text size %.0fX%.0f", self.width, height);
+
+    self.height = height;
+    return height;
+}
+
+@end
